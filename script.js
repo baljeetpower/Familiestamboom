5b9b4f416a36b26c802c49e3efc2c5910e541dd9
baljeetpower <timbesselsen1@gmail.com>
2026-01-05 20:57:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("script werkt");
+const svg = d3.select("#canvas");
+const g = svg.append("g");
+
+// Zoom & pan
+const zoom = d3.zoom()
+  .scaleExtent([0.3, 5])
+  .on("zoom", (event) => {
+    g.attr("transform", event.transform);
+  });
+
+svg.call(zoom);
+
+// Test-element
+g.append("circle")
+  .attr("cx", 0)
+  .attr("cy", 0)
+  .attr("r", 40)
+  .attr("fill", "white");
