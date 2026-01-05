26c172da1fb25dd910e22555113a3ef6a9d45b60
baljeetpower <timbesselsen1@gmail.com>
2026-01-05 21:00:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,9 +10,29 @@ const zoom = d3.zoom()
 
 svg.call(zoom);
 
-// Test-element
-g.append("circle")
-  .attr("cx", 0)
-  .attr("cy", 0)
-  .attr("r", 40)
-  .attr("fill", "white");
+// Data laden
+d3.json("data.json").then(data => {
+
+  const person = g.selectAll(".person")
+    .data(data.people)
+    .enter()
+    .append("g")
+    .attr("class", "person")
+    .attr("transform", d => `translate(${d.x}, ${d.y})`);
+
+  // Foto
+  person.append("image")
+    .attr("href", d => d.photo)
+    .attr("width", 120)
+    .attr("height", 120)
+    .attr("x", -60)
+    .attr("y", -60);
+
+  // Naam
+  person.append("text")
+    .attr("y", 80)
+    .attr("text-anchor", "middle")
+    .attr("fill", "#e0e0e0")
+    .attr("font-size", "14px")
+    .text(d => d.name);
+});
